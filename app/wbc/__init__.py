39f39d2a00feab8fd245d9993a7a6bea846851d5
Maciej Brencz <maciej.brencz@gmail.com>
2016-07-28 22:14:06 +0200

    app.errorhandler requires an exception arg

--- app/wbc/__init__.py
+++ app/wbc/__init__.py
@@ -39,7 +39,7 @@ def handle_bad_api_request(e):
 
 
 @app.errorhandler(404)
-def handle_not_found():
+def handle_not_found(e):
     # API requests - return error messages as JSON
     if request.path.startswith('/api'):
         return handle_bad_api_request(WBCApiError('API end-point not found', 404))
