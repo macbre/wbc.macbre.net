83c2b769fb46b6f68092ad7c9c07668a271c9696
Maciej Brencz <maciej.brencz@gmail.com>
2016-08-08 19:16:56 +0200

    Search: improve errors reporting in HTML mode

--- app/wbc/views/html/search.py
+++ app/wbc/views/html/search.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import render_template
 from flask.views import MethodView
 
@@ -11,6 +12,9 @@ class SearchHTML(MethodView, SearchableMixin):
         if not self.is_searchable():
             raise WBCHtmlError(u'Podaj temat wyszukiwania', 400)
 
-        results, stats = self._get_results(self._get_search_query())
+        try:
+            results, stats = self._get_results(self._get_search_query())
+        except:
+            raise WBCHtmlError(u'Wystąpil błąd w trakcie wyszukiwania', 500)
 
         return render_template('search.html', results=results, stats=stats, query=self._get_search_query())
