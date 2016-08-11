16ca41ecf108063368517a54febc2bd542f27bf6
Maciej Brencz <maciej.brencz@gmail.com>
2016-08-11 20:50:51 +0200

    Set GIT_HASH env and show commit hash in the footer

--- app/wbc/app.py
+++ app/wbc/app.py
@@ -1,8 +1,11 @@
 from os import environ
 
-from . import app
-from . import is_debug
+from . import app, is_debug
+from .common import get_app_version
 
+# emit git hash and register a helper function for templates
+app.logger.info('{} is now running using code {}'.format(app.name, get_app_version()))
+app.jinja_env.globals.update(get_app_version=get_app_version)
 
 app.run(
     debug=is_debug,
--- app/wbc/common.py
+++ app/wbc/common.py
@@ -1,4 +1,14 @@
 import logging
+from os import environ
+
+
+def get_app_version():
+    """
+    Get the short (7 chars) git commit hash of the current code version
+
+    :rtype: str
+    """
+    return environ.get('GIT_HASH', 'dev')[0:7]
 
 
 class LoggableMixin(object):
